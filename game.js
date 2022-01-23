e0c0fad65839283269e69745b0226291b0fc9425
darineabohamdan <darine.abohamdan@gmail.com>
2022-01-23 19:50:57 +0200

    Transformed the background to red ,and a message 'you lost' appear when touching boundaries.

--- game.js
+++ game.js
@@ -1,32 +1,36 @@
-window.onload=function(){
- 
-//variables needed in javaScript: 
-var boundaries=document.getElementsByClassName("boundary");
-var start=document.getElementById("start");
-var status=document.getElementById("status");
-var end=document.getElementById("end");
-var score=0;
-var win=true;
+window.onload = function () {
+
+    //variables needed in javaScript: 
+    var boundaries = document.getElementsByClassName("boundary");
+    var start = document.getElementById("start");
+    var status = document.getElementById("status");
+    var end = document.getElementById("end");
+    var score = 0;
+    var win = true;
 
 
-//Initial score value=0
-boundaries[5].innerHTML="score = " +score;
+    //Initial score value=0
+    boundaries[5].innerHTML = "score = " + score;
 
+    //Start of the MazeGame.
+    //The background becomes red ;and a message "you lost" appear ;when touching the boundaries.
+    start.addEventListener("mouseover", function () {
+        document.getElementById("status").innerHTML = "Move the mouse cursor over to 'E' to win";
+        for (var i = 0; i < boundaries.length; i++) {
+            boundaries[i].addEventListener("mouseover", function () {
+                win = false;
+                for (var j = 0; j < boundaries.length - 1; j++) {
 
+                    boundaries[j].style.backgroundColor = "red";
+                }
+                document.getElementById("status").innerHTML = "You lost!";
+                score -= 10;
+                for (var j = 0; j < boundaries.length; j++) {
+                    boundaries[5].innerHTML = "Score = " + score;
+                }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-}
+                
+            });
+        };
+    });
+};
